6fc589e6d87600313964c6834d56dc5969450aac
0tuedon <90271995+0tuedon@users.noreply.github.com>
2024-08-18 18:48:38 +0100

    chore: 1 getting started

---
+++ 1. Getting-Started/hello_cargo/target/debug/deps/hello_cargo-3939734a17fe0611.d
@@ -0,0 +1,5 @@
+/Users/0tuedon/Documents/GitHub/learning-rust/1. Getting-Started/hello_cargo/target/debug/deps/hello_cargo-3939734a17fe0611: src/main.rs
+
+/Users/0tuedon/Documents/GitHub/learning-rust/1. Getting-Started/hello_cargo/target/debug/deps/hello_cargo-3939734a17fe0611.d: src/main.rs
+
+src/main.rs:
---
+++ 1. Getting-Started/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/0tuedon/Documents/GitHub/learning-rust/1.\ Getting-Started/hello_cargo/target/debug/hello_cargo: /Users/0tuedon/Documents/GitHub/learning-rust/1.\ Getting-Started/hello_cargo/src/main.rs
